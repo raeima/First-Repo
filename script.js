10f4244683783136180ab677cf87a97f3ba7076c
raeima <30875444+raeima@users.noreply.github.com>
2017-08-14 08:55:50 -1000

    Made sayHello ()

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ function sayHello() {
   //I create a variable called userName and ask the user something//
   window.alert("Hello " + userName);
   //I create a popup window teller the userName hello.//
-}
+} 
