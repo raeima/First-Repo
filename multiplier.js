d379ef1c43040845a46d66afed905e16c45e9da1
raeima <30875444+raeima@users.noreply.github.com>
2017-08-16 09:24:16 -1000

    Number multiplier
    Number times number

---
+++ multiplier.js
@@ -0,0 +1,8 @@
+//Ask user for number
+var num1 = prompt("Pick a number");
+//Ask user for another number
+var num2 = prompt("Pick another number");
+//Take both numbers and multiply them and save the output as another variable
+var equals = (num1 * num2);
+//Output the value to the screen using a window popup
+window.alert(equals);
