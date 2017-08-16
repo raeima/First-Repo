886098f88840a80e89d974ce42d0d81300c913cb
raeima <30875444+raeima@users.noreply.github.com>
2017-08-16 08:45:06 -1000

    Added js with favfoods and student names
    Chuck Bacon comes out (comments explain)

---
+++ lettucechoice.js
@@ -0,0 +1,6 @@
+var favFoods = ["Bacon", "lettuce", "Tomato"];
+//put fav foods//
+var students = ["Michael", "Raene", "Chuck", "Ryan"];
+//put some student's names//
+window.alert(students[2] + " " + favFoods[0]);
+//called index number student 2 (chuck) added a space and added favfoods index #0 (bacon) = CHuck Bacon//
