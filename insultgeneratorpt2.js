db93180cd5dee884045790d7c72583ff376864a2
raeima <30875444+raeima@users.noreply.github.com>
2017-08-21 09:05:57 -1000

    Disappointed Generator
    created generator with random disappointed phrases

---
+++ insultgeneratorpt2.js
@@ -0,0 +1,8 @@
+function whatEven() {
+  var shock = ["why'd you click it", "I told you not to", "you are a tomato", "OHMAHGAWD GO AWAY", "AAAAAAAAAAAAAAAHHHHHHHHH", "what the hell"];
+//put some "bruh" sayings//
+  var insulted = ["Bruh", "Boi", "PERSON", "Wow", "Asshole", "White Crayon"];
+//put some "disappointed" words//
+  window.alert(insulted[Math.floor(Math.random() * 6)] + " " + shock[Math.floor(Math.random() * 6)]);
+//called index number student 2 (chuck) added a space and added favfoods index #0 (bacon) = CHuck Bacon//
+}
